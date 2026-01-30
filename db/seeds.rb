2cf048ffd52cdd16640a44364891ae0d6fc51c8c
leopolicastro <43428385+leopolicastro@users.noreply.github.com>
2026-01-29 20:29:17 -0500

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-User.create(email_address: "admin@example.com", password: "abc123")
+User.create(email_address: "admin@example.com", password: "abc123", admin: true)
 
 puts "Created #{User.count} users"
